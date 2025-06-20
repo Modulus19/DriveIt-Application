f1216daea7393b84084403fb33c6944429effa73
Modulus19 <127755804+Modulus19@users.noreply.github.com>
2025-06-20 12:07:33 +0100

    Initial commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,89 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform, Image, StyleSheet } from "react-native";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
+        tabBarLabelStyle: {
+          fontFamily: "PoppinsBold",
+        },
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
+            position: "absolute",
           },
           default: {},
         }),
-      }}>
+      }}
+    >
       <Tabs.Screen
-        name="index"
+        name="branches"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Branches",
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require("../../assets/images/Branches.png")}
+              style={[
+                styles.Logo,
+                {
+                  width: size,
+                  height: size,
+                  tintColor: color,
+                },
+              ]}
+            />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="vehicles"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Vehicles",
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require("../../assets/images/Vehicles.png")}
+              style={[
+                styles.Logo,
+                {
+                  width: size,
+                  height: size,
+                  tintColor: color,
+                },
+              ]}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: "Account",
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require("../../assets/images/Account.png")}
+              style={[
+                styles.Logo,
+                {
+                  width: size,
+                  height: size,
+                  tintColor: color,
+                },
+              ]}
+            />
+          ),
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  Logo: {
+    width: 23,
+    height: 28,
+    resizeMode: "contain",
+    tintColor: "#000",
+  },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,34 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { useFonts } from "expo-font";
+import { Stack, SplashScreen } from "expo-router";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    PoppinsRegular: require("../assets/fonts/Poppins/Poppins-Regular.ttf"),
+    PoppinsBold: require("../assets/fonts/Poppins/Poppins-Bold.ttf"),
+    PoppinsSemiBold: require("../assets/fonts/Poppins/Poppins-SemiBold.ttf"),
+    PoppinsMedium: require("../assets/fonts/Poppins/Poppins-Medium.ttf"),
+    PoppinsLight: require("../assets/fonts/Poppins/Poppins-Light.ttf"),
   });
 
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
   if (!loaded) {
-    // Async font loading only occurs in development.
     return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(auth)" />
+      <Stack.Screen name="(tabs)" />
+    </Stack>
   );
 }
